0a4a0252f5eb603a8460ca86bb76ab887a985b30
Gucci <bokuricci@gmail.com>
2017-09-13 01:14:13 +0900

    20170912_003 リンクの問題を解決

--- ACsite/ac_site/views.py
+++ ACsite/ac_site/views.py
@@ -6,7 +6,7 @@ from django.template import loader
 
 import datetime
 
-from .models import Pref_table
+from .models import Pref_table, Company_table, Member_table
 
 
 def index(request):
@@ -17,56 +17,53 @@ def index(request):
     # ヘッダーフィールドをつける：response = HttpResponse()
     #                        response[20] = 120 など
 
-    latest_pref_list = Pref_table.objects.order_by('-pf_id')[:5]
+    company_info = Company_table.objects.order_by('-comp_id')[:1]
+    member_info = Member_table.objects.order_by('member_id')[:1]
     context = {
-        'latest_pref_list': latest_pref_list,
+        'company_info': company_info,
+        'member_info': member_info,
     }
     # render(arg1, arg2, arg3)
     # arg1: requestオブジェクト
     # arg2: テンプレート名
     # arg3: オプション辞書（= DB変数と.pyの変数の紐付け）
     return render(request, 'ac_site/index.html', context)
-    #latest_pref_list = Pref_table.objects.order_by('-pf_id')[:5]
-    #output = ', '.join([q.pref for q in latest_pref_list])
-    #return HttpResponse(output)
-    #return HttpResponse("You're looking at question1 ")
 
 
 def company(request):
     '''会社情報を取得する（静的）'''
-    context = [
-        'company_name': "ログトレ株式会社",
-        'address': "東京都 港区 東麻布1-7-3 第二渡邊ビル7F",
-        'phone': "050-5309-7463",
-        'google-map': "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3241.7856984965265!2d139.7435944!3d35.6576512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1504944273102",
-        'chair': "田口 翔一",
-        'stock': "200万円",
-    ]
+    company_info = Company_table.objects.order_by('-comp_id')[:1]
+    context = {
+        'company_info': company_info,
+    }
     return render(request, 'ac_site/company.html', context)
 
 
 def contact(request):
-    '''お問い合わせ情報を取得する（静的）'''
-    context = 0 # 仮置きで変数を定義
+    '''お問い合わせ時に表示する会社情報を取得する（静的）'''
+    company_info = Company_table.objects.order_by('-comp_id')[:1]
+    context = {
+        'company_info': company_info,
+    }
     return render(request, 'ac_site/contact.html', context)
 
 
-def login(request):
-    '''ログイン情報を取得する（静的）'''
-    context = 0 # 仮置きで変数を定義
-    return render(request, 'ac_site/login.html', context)
+#def login(request):
+#    '''ログイン情報を取得する（静的）'''
+#    context = 0 # 仮置きで変数を定義
+#    return render(request, 'ac_site/login.html', context)
 
 
-def minnpaku_news(request):
-    '''民泊日記情報を取得する（静的）'''
-    context = 0 # 仮置きで変数を定義
-    return render(request, 'ac_site/minnpaku_news.html', context)
+#def minnpaku_news(request):
+#    '''民泊日記情報を取得する（静的）'''
+#    context = 0 # 仮置きで変数を定義
+#    return render(request, 'ac_site/minnpaku_news.html', context)
 
 
-def news_release(request):
-    '''ニュースリリース情報を取得する（静的）'''
-    context = 0 # 仮置きで変数を定義
-    return render(request, 'ac_site/news_release.html', context)
+#def news_release(request):
+#    '''ニュースリリース情報を取得する（静的）'''
+#    context = 0 # 仮置きで変数を定義
+#    return render(request, 'ac_site/news_release.html', context)
 
 
 def prefs(request):
@@ -78,10 +75,10 @@ def prefs(request):
     return render(request, 'ac_site/prefs.html', context)
 
 
-def registration(request):
-    '''サインイン情報を取得する（静的）'''
-    context = 0 # 仮置きで変数を定義
-    return render(request, 'ac_site/registration.html', context)
+#def registration(request):
+#    '''サインイン情報を取得する（静的）'''
+#    context = 0 # 仮置きで変数を定義
+#    return render(request, 'ac_site/registration.html', context)
 
 
 def current_datetime(request):
